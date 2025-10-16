194f52405e0822c31fbb3b42cf8555ba8c68d678
Lir09 <codingsnowflower@gmail.com>
2025-10-16 10:33:54 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ✅ 여기 변경
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
