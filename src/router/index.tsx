3542218c85e499ac5692f7153fb6758374781ae5
Lir09 <codingsnowflower@gmail.com>
2025-10-16 10:01:47 +0900

    끝

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,13 @@
+// src/router/index.tsx
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import login from "../pages/Login";
+import App from "../App"; // ✅ 이거 꼭 추가
+import Login from "../pages/Login"; // ✅ 대문자 컴포넌트
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />, // 공통 레이아웃
-    children: [{ path: "", element: <login /> }],
+    children: [{ path: "", element: <Login /> }],
   },
 ]);
 
