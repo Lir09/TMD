12d7dd29fb47b946eaa82330cbb9f405a72d8043
Lir09 <codingsnowflower@gmail.com>
2025-10-16 10:35:13 +0900

    d

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+    plugins: [react()],
+    base: "/TMD/", // ✅ 깃허브 리포지토리 이름과 동일하게!
+});
